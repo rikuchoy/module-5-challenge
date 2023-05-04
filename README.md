c942ad9ef41fc28d5e166e26959610f4bc899c40
Riku Choy <rikuchoy@gmail.com>
2023-05-04 16:17:03 -0700

    updated readme file, linked scripts to html properly

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# week-5-challenge
+# week-5-challenge: Work Day Scheduler (Third Party API's)
+
+## User Story
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+
+
+## Acceptance Criteria (from Challenge ReadMe)
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+![example screenshot](assets/s.png)
+
+## Link to Page
+[Click to view page](https://rikuchoy.github.io/week-4-challenge/)
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -20,29 +20,29 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
+
   
 
 // time elements
-var time9am = $("#9AM");
-var time10am = $("#10AM");
-var time11am = $("#11AM");
-var time12pm = $("#12PM");
-var time1pm = $("#1PM");
-var time2pm = $("#2PM");
-var time3pm = $("#3PM");
-var time4pm = $("#4PM");
-var time5pm = $("#5PM");
+let time9am = $("#9AM");
+let time10am = $("#10AM");
+let time11am = $("#11AM");
+let time12pm = $("#12PM");
+let time1pm = $("#1PM");
+let time2pm = $("#2PM");
+let time3pm = $("#3PM");
+let time4pm = $("#4PM");
+let time5pm = $("#5PM");
 
 timeElArr = [time9am, time10am, time11am, time12pm, time1pm,
             time1pm, time2pm, time3pm, time4pm, time5pm];
 
 function timeUpdate() {
-    var today = dayjs();
+    let today = dayjs();
 
-    $('#currentDay').text(today.format("dddd, MMMM DD YYYY, h:mm.ss"));
+    $('#currentDay').text(today.format("dddd, MMMM DD YYYY, h:mm:ss"));
 
-    var current = dayjs().format('HH');
+    let current = dayjs().format('HH');
     for (let i = 0; i < timeElArr.length; i++){
         timeElArr[i].removeClass("future", "past", "present");
         if (current > timeElArr[i].data('hour')) {
@@ -55,20 +55,30 @@ function timeUpdate() {
     }
   }
 
+timeUpdate();
+setInterval(timeUpdate, 1000); //update time every 1000 ms
+
 // saving to local storage/rendering saved items
-var saveBtn = $(".saveBtn");
+let saveBtn = $(".saveBtn");
 
 function Submit(event) {
     event.preventDefault();
     
-    var btnClicked = $(event.currentTarget);
-    var textTarget = btnClicked.siblings("textarea")
+    let btnClicked = $(event.currentTarget);
+    let textareaTarget = btnClicked.siblings("textarea");
+    let timeblockTarget = textareaTarget.data("hour");
+    localStorage.setItem("time block " + timeblockTarget, textareaTarget.val());
 
 }
 
+saveBtn.on('click', Submit);
 
-// calling functions
-timeUpdate();
-setInterval(timeUpdate, 1000); //update time every 1000 ms
+function renderLastSaved() {
+    for (let el of timeElArr) {
+        el.val(localStorage.getItem("time block " + el.data(hour)));
+    }
+}
+
+renderLastSaved();
 
 
