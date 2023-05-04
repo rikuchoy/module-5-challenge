24c5d16eef756a210f8ba2ba7cc78efdc980893e
Riku Choy <rikuchoy@gmail.com>
2023-05-04 15:16:18 -0700

    added time element and formed array in js

--- assets/script.js
+++ assets/script.js
@@ -20,8 +20,55 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-    var currentdate = dayjs().format("MMM dd, YYYY");
-    $("#currentday").text(currentdate);
-    // repeat ever 1000 ms
   });
   
+
+// time elements
+var time9am = $("#9AM");
+var time10am = $("#10AM");
+var time11am = $("#11AM");
+var time12pm = $("#12PM");
+var time1pm = $("#1PM");
+var time2pm = $("#2PM");
+var time3pm = $("#3PM");
+var time4pm = $("#4PM");
+var time5pm = $("#5PM");
+
+timeElArr = [time9am, time10am, time11am, time12pm, time1pm,
+            time1pm, time2pm, time3pm, time4pm, time5pm];
+
+function timeUpdate() {
+    var today = dayjs();
+
+    $('#currentDay').text(today.format("dddd, MMMM DD YYYY, h:mm.ss"));
+
+    var current = dayjs().format('HH');
+    for (let i = 0; i < timeElArr.length; i++){
+        timeElArr[i].removeClass("future", "past", "present");
+        if (current > timeElArr[i].data('hour')) {
+            timeElArr[i].addClass("past");
+        }else if (now === timeElArr[i].attr('data-hour')) {
+            timeElArr[i].addClass("present");
+        }else {
+            timeElArr[i].addClass("future");
+        }
+    }
+  }
+
+// saving to local storage/rendering saved items
+var saveBtn = $(".saveBtn");
+
+function Submit(event) {
+    event.preventDefault();
+    
+    var btnClicked = $(event.currentTarget);
+    var textTarget = btnClicked.siblings("textarea")
+
+}
+
+
+// calling functions
+timeUpdate();
+setInterval(timeUpdate, 1000); //update time every 1000 ms
+
+
